5e60b2a2625851475bed645059b0be13c316befe
thaolv-1565 <luu.van.thao@sun-asterisk.com>
2020-05-24 17:49:10 +0700

    Update BaseActivity

--- app/src/main/java/com/example/mvvm_kotlin_architect/views/BaseActivity.kt
+++ app/src/main/java/com/example/mvvm_kotlin_architect/views/BaseActivity.kt
@@ -4,10 +4,24 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mvvm_kotlin_architect.viewmodels.BaseViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+abstract class BaseActivity<T : BaseViewModel> : AppCompatActivity() {
+    protected lateinit var mViewModel: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+        mViewModel = getViewModel()
+        initViews()
+        observeData()
+
     }
+
+    abstract fun getLayoutId(): Int
+
+    abstract fun getViewModel(): T
+
+    abstract fun observeData()
+
+    abstract fun initViews()
 }
--- app/src/main/java/com/example/mvvm_kotlin_architect/views/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_kotlin_architect/views/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.mvvm_kotlin_architect.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.example.mvvm_kotlin_architect.R
 import com.example.mvvm_kotlin_architect.viewmodels.HomeViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
-    val viewModel: HomeViewModel by viewModel()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.txtDummyText).setText(viewModel.giveHelloString())
+class MainActivity : BaseActivity<HomeViewModel>() {
+
+    override fun getViewModel(): HomeViewModel {
+        val mViewModel: HomeViewModel by viewModel()
+        return mViewModel
     }
+
+    override fun getLayoutId(): Int = R.layout.activity_main
+    override fun observeData() {
+        txtDummyText.text = mViewModel.giveHelloString()
+    }
+
+    override fun initViews() {
+    }
+
 }
